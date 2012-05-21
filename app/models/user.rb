b742ade473171deff408edc6abbdfcccf5a1ed2c
Sam Oliver <sam@samoliver.com>
2012-05-21 16:47:44 +0100

    Move s3 config to model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,13 @@
 class User < ActiveRecord::Base
 	has_attached_file :photo,
 		:storage => :s3,
-		:bucket => 'shared_workforce_demo',
-    :s3_credentials => "#{Rails.root}/config/s3.yml",
-    :path => "/:style/:id/:filename"
-
+		:path => "/:style/:id/:filename",
+    :s3_credentials => {
+    	:access_key_id => ENV['S3_KEY'],
+    	:secret_access_key => ENV['S3_SECRET'],
+    	:bucket => ENV['S3_BUCKET']
+    }
+    
   after_save :tag_photo, :if=>:photo_updated_at_changed?
 
   def tag_photo
