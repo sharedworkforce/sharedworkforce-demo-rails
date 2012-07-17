befbf3207deeb6f7f256cbc7456904a944ddad8b
Sam Oliver <sam@samoliver.com>
2012-07-17 12:00:11 +0100

    Incude notification email in fixtures

--- test/factories/cats.rb
+++ test/factories/cats.rb
@@ -4,5 +4,6 @@ FactoryGirl.define do
 	factory :cat do |c|
 		description "Factory description"
 		image_url "http://example.com/factory_image.jpg"
+		notification_email "test@example.com"
 	end
 end
